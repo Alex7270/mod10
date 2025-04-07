dde917955422558d7d2fc713a9bff9a486d8a777
Alexey Kultaev <kav7270@gmail.com>
2025-04-08 00:49:53 +1000

    В модуле widget.py создана фуркция get_date. Корректировка main.py

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
@@ -13,3 +15,11 @@ def mask_account_card(number: str) -> str:
         return f"{' '.join(number_lst[:2])} {get_mask_card_number(number_lst[2])}"
     else:
         return f"{number_lst[0]} {get_mask_card_number(number_lst[1])}"
+
+
+def get_date(date: str) -> str:
+    """
+    Функция преобразования формата даты
+    """
+    date_str = date[:10]
+    return datetime.strptime(date_str, '%Y-%m-%d').date().strftime('%d.%m.%Y')
