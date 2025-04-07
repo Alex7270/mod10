1403f3ee9cf8a91e7bd8b0475ed1de8aae65cc42
Alexey Kultaev <kav7270@gmail.com>
2025-04-07 22:07:56 +1000

    Создан модуль widget, добавлена функция mask_account_card. Корректировка main.py

---
+++ src/widget.py
@@ -0,0 +1,15 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(number: str) -> str:
+    """
+    Функция маскировки номера банковской карты или счета
+    """
+    number_lst = number.strip().split(" ")
+
+    if number_lst[0].lower() == "счет":
+        return f"{number_lst[0]} {get_mask_account(number_lst[1])}"
+    elif len(number_lst) > 2:
+        return f"{' '.join(number_lst[:2])} {get_mask_card_number(number_lst[2])}"
+    else:
+        return f"{number_lst[0]} {get_mask_card_number(number_lst[1])}"
