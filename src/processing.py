9a66227d61c54d2a22b84d3562c8e96a10eb59aa
Alexey Kultaev <kav7270@gmail.com>
2025-04-16 22:34:28 +1000

    feat: add function sort_by_date in processing.py, modified: main.py

--- src/processing.py
+++ src/processing.py
@@ -3,3 +3,10 @@ def filter_by_state(my_list: list[dict[str, int | str]], state: str = "EXECUTED"
     Функция фильтрует входной список по ключу
     """
     return [my_dict for my_dict in my_list if my_dict.get("state") == state]
+
+
+def sort_by_date(list_dict: list[dict[str, int | str]], sort: bool = True) -> list[dict[str, int | str]]:
+    """
+    Функция сортирует список по дате
+    """
+    return sorted(list_dict, key=lambda x: x["date"], reverse=sort)
