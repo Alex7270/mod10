2bb200c99c2bf8e7e782ccbf71da0b71a8c67eb3
Alexey Kultaev <kav7270@gmail.com>
2025-04-19 11:21:23 +1000

    docs: added Docstring parameter description to filter_by_state function in modul processing

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,9 @@
 def filter_by_state(my_list: list[dict[str, int | str]], state: str = "EXECUTED") -> list[dict[str, int | str]]:
     """
     Функция фильтрует входной список по ключу
+    :param my_list: list[dict[str, int | str]
+    :param state: str
+    :return: list[dict[str, int | str]]
     """
     return [my_dict for my_dict in my_list if my_dict.get("state") == state]
 
