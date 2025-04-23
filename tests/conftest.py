120a675142df59db4463e839e55a9198bcf284ee
Alexey Kultaev <kav7270@gmail.com>
2025-04-23 22:28:12 +1000

    add test_widget.py add func test_mask_account_card_correct, update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,3 +26,17 @@ def account_number_incorrect() -> list[str]:
         "hhhh____k kkk//  1234",
         "55555gggIIII1228  --",
     ]
+
+
+@pytest.fixture
+def correct_number() -> list[str]:
+    return [
+        "Maestro 1596837868705199",
+        "Счет 64686473678894779589",
+        "MasterCard 7158300734726758",
+        "Счет 35383033474447895560",
+        "Visa Classic 6831982476737658",
+        "Visa Platinum 8990922113665229",
+        "Visa Gold 5999414228426353",
+        "Счет 73654108430135874305",
+    ]
