0075c30bbfb7375f46ca066fe1afb7789b8cca57
Alexey Kultaev <kav7270@gmail.com>
2025-04-27 22:37:32 +1000

    add funcs test_sort_by_date_default in modul test_processing and conftest.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,6 +1,8 @@
+from typing import Any
+
 import pytest
 
-from src.processing import filter_by_state
+from src.processing import filter_by_state, sort_by_date
 
 
 @pytest.mark.parametrize(
@@ -83,3 +85,33 @@ def test_filter_by_state(
     :return: None
     """
     assert filter_by_state(user_list, state) == expected
+
+
+@pytest.mark.parametrize(
+    "list_dict, expected",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+    ],
+)
+def test_sort_by_date_default(
+    list_dict: list[dict[str, Any]], expected: list[dict[str, int | str]], sort: bool = True
+) -> None:
+    """
+    Функция тестирования сортировки списка словарей по датам в порядке убывания
+    :param list_dict: list[dict[str, Any]      :param expected: expected: list[dict[str, int | str]
+    :return: None
+    """
+    assert sort_by_date(list_dict, sort) == expected
