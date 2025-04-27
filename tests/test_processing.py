4e8d38caeeac5629cd46def05cc238b63935e94e
Alexey Kultaev <kav7270@gmail.com>
2025-04-27 19:33:51 +1000

    update funcs conftest.py, add test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -24,9 +24,52 @@ def test_filter_by_state_default(
     my_list: list[dict[str, int | str]], expected: list[dict[str, int | str]], state: str = "EXECUTED"
 ) -> None:
     """
-    Функция тестирования фильтрации списка словарей по заданному статусу state
+    Функция тестирования фильтрации списка словарей без указания статуса
     :param my_list: list[dict[str, int | str]]
     :param expected: list[dict[str, int | str]]     :param state: str
     :return: None
     """
     assert filter_by_state(my_list, state) == expected
+
+
+@pytest.mark.parametrize(
+    "user_list, state, expected",
+    [
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "CANCELED",
+            [
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+        ),
+        (
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            ],
+            "EXECUTED",
+            [
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+    ],
+)
+def test_filter_by_state(
+    user_list: list[dict[str, int | str]], expected: list[dict[str, int | str]], state: str
+) -> None:
+    """
+    Функция тестирования фильтрации списка словарей по заданному статусу state
+    :param user_list: list[dict[str, int | str]]
+    :param expected: list[dict[str, int | str]]     :param state: str
+    :return: None
+    """
+    assert filter_by_state(user_list, state) == expected
