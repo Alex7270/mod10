3d59e80db0bcec26dca06a253d5427673b96233d
Alexey Kultaev <kav7270@gmail.com>
2025-05-10 09:22:13 +1000

    add data, update decorators

--- src/decorators.py
+++ src/decorators.py
@@ -3,17 +3,37 @@ from functools import wraps
 from typing import Any, Callable
 
 
-def log() -> Callable[[Any], Callable[[int, int], Any]]:
+def log(filename: str | None = None) -> Callable[[Any], Callable[[int, int], Any]]:
     def my_decorator(func: Any) -> Callable[[int, int], Any]:
         @wraps(func)
         def wrapper(*args: int, **kwargs: int) -> Any:
-            result = func(*args, **kwargs)
-            print(
-                f"Начало работы функции: {datetime.now()}\n\nИмя функции: {func.__name__}\n{func.__doc__}\n"
-                f"Аргументы функции args: {args}; kwargs: {kwargs}\n\nОкончание работы функции: {datetime.now()}\n\n"
-                f"Результат работы функции:\n"
-            )
-            return result
+            try:
+                datetime.now()
+                result = func(*args, **kwargs)
+                datetime.now()
+                message = (
+                    f"Начало работы функции: {datetime.now()}\n\n"
+                    f"Имя функции: {func.__name__}\n{func.__doc__}\n"
+                    f"Аргументы функции args: {args}; kwargs: {kwargs}\n\n"
+                    f"Окончание работы функции: {datetime.now()}\n\nРезультат работы функции: OK\n"
+                )
+                if filename:
+
+                    with open("data/" + filename, "a", encoding="utf-8") as file:
+                        file.write(message)
+                else:
+                    print(message)
+
+                return result
+
+            except Exception as e:
+                message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                if filename:
+                    with open("data/" + filename, "a") as file:
+                        file.write(message)
+                else:
+                    print(message)
+            raise
 
         return wrapper
 
