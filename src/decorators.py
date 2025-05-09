e6f323c88ab3f8a4334441b1e527c582aa822540
Alexey Kultaev <kav7270@gmail.com>
2025-05-09 20:11:18 +1000

    apdate func log in modul decorators

--- src/decorators.py
+++ src/decorators.py
@@ -3,17 +3,16 @@ from functools import wraps
 from typing import Any, Callable
 
 
-def log(filename: str = "mylog.txt") -> Callable[[Any], Callable[[int, int], Any]]:
+def log() -> Callable[[Any], Callable[[int, int], Any]]:
     def my_decorator(func: Any) -> Callable[[int, int], Any]:
         @wraps(func)
         def wrapper(*args: int, **kwargs: int) -> Any:
-            print(f"Начало работы функции {datetime.now()}")
-            print(f"Имя функции {func.__name__}")
-            print(f"Аргументы декоратора: {filename}")
-            print(func.__doc__)
             result = func(*args, **kwargs)
-            print(f"Окончание работы функции {datetime.now()}")
-            print("Результат работы функции:")
+            print(
+                f"Начало работы функции: {datetime.now()}\n\nИмя функции: {func.__name__}\n{func.__doc__}\n"
+                f"Аргументы функции args: {args}; kwargs: {kwargs}\n\nОкончание работы функции: {datetime.now()}\n\n"
+                f"Результат работы функции:\n"
+            )
             return result
 
         return wrapper
