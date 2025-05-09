b23860b5d6e44b4b0471a6c970e170cdaf25c83d
Alexey Kultaev <kav7270@gmail.com>
2025-05-09 14:37:49 +1000

    update log in modul decorators

--- src/decorators.py
+++ src/decorators.py
@@ -1,12 +1,19 @@
 from datetime import datetime
+from functools import wraps
+from typing import Any, Generator
 
 
-def log(filename="mylog.txt"):
-    def my_decorator(function):
+def log(filename: str = "mylog.txt"):
+    def my_decorator(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
-            print(datetime.now())
-            result = function(*args, **kwargs)
-            print(datetime.now())
+            print(f"Начало работы функции {datetime.now()}")
+            print(f"Имя функции {func.__name__}")
+            print(f"Аргументы декоратора: {filename}")
+            print(func.__doc__)
+            result = func(*args, **kwargs)
+            print(f"Окончание работы функции {datetime.now()}")
+            print("Результат работы функции:")
             return result
 
         return wrapper
