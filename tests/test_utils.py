a918a627224d67f29403fed5a6721ded73d09685
Alexey Kultaev <kav7270@gmail.com>
2025-05-20 15:18:05 +1000

    update func test_get_transaction in module test_utils and add fixture in modul conftest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,13 @@
-def test_get_transaction(path: str) -> None:
+from typing import Any
+
+from src.utils import get_transaction
+
+
+def test_get_transaction_correct(path: str, expected_correct: list[dict[str, Any]]) -> None:
+    """
+    Тестирование функции с корректными данными о финансовых транзакциях
+    :param path: str
+    :return: None
     """
 
-    :param path:
-    :return:
-    """
+    assert get_transaction(path) == expected_correct
