5858f7ce73cdaa86e0ba950fa2a38ef4bca457ce
Alexey Kultaev <kav7270@gmail.com>
2025-05-20 16:34:20 +1000

    add func in module test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,3 +11,21 @@ def test_get_transaction_correct(path: str, expected_correct: list[dict[str, Any
     """
 
     assert get_transaction(path) == expected_correct
+
+
+def test_get_transaction_incorrect() -> None:
+    """
+    Тестирование функции с не корректными данными пути к файлу
+    :return:
+    """
+
+    assert get_transaction("") == []
+
+
+def test_get_transaction_empty() -> None:
+    """
+    Тестирование функции с не корректными данными о финансовых транзакциях
+    :return:
+    """
+
+    assert get_transaction("data/operation_empty.json") == []
