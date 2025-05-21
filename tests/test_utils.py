5f88d8777bc27a2016a3ff247130d067a8878c65
Alexey Kultaev <kav7270@gmail.com>
2025-05-21 14:02:06 +1000

    fixed errors funcs in module test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,31 +1,43 @@
 from typing import Any
+from unittest.mock import patch
 
 from src.utils import get_transaction
 
 
-def test_get_transaction_correct(path: str, expected_correct: list[dict[str, Any]]) -> None:
+@patch("json.load")
+def test_get_transaction_correct(mock_loads: Any, path: str, expected_correct: list[dict[str, Any]]) -> None:
     """
     Тестирование функции с корректными данными о финансовых транзакциях
+    :param mock_loads: Any
     :param path: str
+    :param expected_correct: list[dict[str, Any]]
     :return: None
     """
-
+    mock_loads.return_value = expected_correct
     assert get_transaction(path) == expected_correct
+    mock_loads.assert_called_once()
 
 
-def test_get_transaction_incorrect() -> None:
+@patch("json.load")
+def test_get_transaction_incorrect(mock_loads: Any, path_incorrect: str) -> None:
     """
     Тестирование функции с не корректными данными пути к файлу
-    :return:
+    :param mock_loads: Any
+    :param path_incorrect: str
+    :return: None
     """
+    mock_loads.return_value = []
+    assert get_transaction(path_incorrect) == []
 
-    assert get_transaction("") == []
 
-
-def test_get_transaction_empty() -> None:
+@patch("json.load")
+def test_get_transaction_empty(mock_loads: Any, path_file_empty: str) -> None:
     """
-    Тестирование функции с не корректными данными о финансовых транзакциях
-    :return:
+    Тестирование функции с некорректными данными о финансовых транзакциях
+    :param mock_loads: Any
+    :param path_file_empty: str
+    :return: None
     """
-
-    assert get_transaction("data/operation_empty.json") == []
+    mock_loads.return_value = []
+    assert get_transaction(path_file_empty) == []
+    mock_loads.assert_called_once()
