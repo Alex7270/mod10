64d99fb6404e858d1795f0fc03c3ae1801971f67
Alexey Kultaev <kav7270@gmail.com>
2025-05-27 18:58:13 +1000

    add module logger

--- main.py
+++ main.py
@@ -8,80 +8,84 @@ from src.widget import get_date, mask_account_card
 
 
 def main() -> None:
-    print(mask_account_card("Maestro 1596837868705199"))
-
-    print(mask_account_card("Счет 64686473678894779589"))
-
-    print(mask_account_card("MasterCard 7158300734726758"))
-
-    print(mask_account_card("Счет 35383033474447895560"))
-
-    print(mask_account_card("Visa Classic 6831982476737658"))
-
-    print(mask_account_card("Visa Platinum 8990922113665229"))
-
-    print(mask_account_card("Visa Gold 5999414228426353"))
-
-    print(mask_account_card("Счет 73654108430135874305"))
-
-    print(get_date("2024-03-11T02:26:18.671407"))
-
-    print(
-        filter_by_state(
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ]
-        )
-    )
-
-    print(
-        sort_by_date(
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ]
-        )
-    )
-
-    print()
-
-    usd_transactions = filter_by_currency(transactions, "USD")
-    for i in usd_transactions:
-        print(i)
-
-    print()
-
-    rub_transactions = filter_by_currency(transactions, "RUB")
-    for i in rub_transactions:
-        print(i)
-
-    print()
-
-    descriptions = transaction_descriptions(transactions)
+    # print(mask_account_card("Maestro 1596837868705199"))
+    #
+    # print(mask_account_card("Счет 64686473678894779589"))
+    #
+    # print(mask_account_card("MasterCard 7158300734726758"))
+    #
+    # print(mask_account_card("Счет 35383033474447895560"))
+    #
+    # print(mask_account_card("Visa Classic 6831982476737658"))
+    #
+    # print(mask_account_card("Visa Platinum 8990922113665229"))
+    #
+    # print(mask_account_card("Visa Gold 5999414228426353"))
+    #
+    # print(mask_account_card("Счет 73654108430135874305"))
+    #
+    # print(get_date("2024-03-11T02:26:18.671407"))
+    #
+    # print(
+    #     filter_by_state(
+    #         [
+    #             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    #             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    #             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    #             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    #         ]
+    #     )
+    # )
+    #
+    # print(
+    #     sort_by_date(
+    #         [
+    #             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    #             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    #             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    #             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    #         ]
+    #     )
+    # )
+    #
+    # print()
+    #
+    # usd_transactions = filter_by_currency(transactions, "USD")
+    # for i in usd_transactions:
+    #     print(i)
+    #
+    # print()
+    #
+    # rub_transactions = filter_by_currency(transactions, "RUB")
+    # for i in rub_transactions:
+    #     print(i)
+    #
+    # print()
+    #
+    # descriptions = transaction_descriptions(transactions)
+    # for i in descriptions:
+    #     print(i)
+    #
+    # print()
+    #
+    # for card_number in card_number_generator(1, 5):
+    #     print(card_number)
+    #
+    # print()
+    #
+    print(get_transaction("data/operations.json"))
+    #
+    # print()
+    l = set()
+    a = read_transactions_csv("data/transactions.csv")
+    descriptions = transaction_descriptions(a)
     for i in descriptions:
-        print(i)
+        l.add(i)
+    print(l)
 
-    print()
-
-    for card_number in card_number_generator(1, 5):
-        print(card_number)
-
-    print()
-
-    get_transaction("data/operations.json")
-
-    print()
-
-    pprint(read_transactions_csv("data/transactions.csv"), indent=4, sort_dicts=False)
-
-    print()
-
-    pprint(read_transactions_xlsx("data/transactions_excel.xlsx"), indent=4, sort_dicts=False)
+    print(filter_by_state(a))
+    #
+    # pprint(read_transactions_xlsx("data/transactions_excel.xlsx")[:1], indent=4, sort_dicts=False)
 
 
 if __name__ == "__main__":
