d296103675d61088832782b78bf624741381e536
Alexey Kultaev <kav7270@gmail.com>
2025-05-29 07:54:37 +1000

    add test_filters in tests

---
+++ tests/test_filters.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+from src.filters import filter_transactions
+
+
+def test_filter_transactions(transactions: list[dict[str, Any]], expected_filter) -> None:
+    """
+    Тестирование функции фильтрации транзакций по заданному ключевому слову
+    :param transactions: list[dict[str, Any]]
+    :return: None
+    """
+    assert filter_transactions(transactions, "перевод") == expected_filter
+
+
+def test_filter_transactions_no_search(transactions: list[dict[str, Any]], expected_filter) -> None:
+    """
+    Тестирование функции фильтрации транзакций при отсутствии ключевого слова в транзакции
+    :param transactions: list[dict[str, Any]]
+    :return: None
+    """
+    assert filter_transactions(transactions, "kkkkkk") == []
