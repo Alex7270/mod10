a2cc69dc877818d2f599c92d9d8c5bcd5b77ff10
Alexey Kultaev <kav7270@gmail.com>
2025-05-28 22:04:41 +1000

    doc: update docstring

--- src/filters.py
+++ src/filters.py
@@ -7,6 +7,7 @@ def filter_transactions(transactions: list[dict[str, Any]], search: str) -> list
     Фильтрация транзакций по заданному ключевому слову
     :param transactions: list[dict[str, Any]]
     :param search: str
+    :return: list[dict[str, Any]]
     """
     new_lst = []
     for transaction in transactions:
