42514e905e3f8c918c63850df843a4067a91cc66
Alexey Kultaev <kav7270@gmail.com>
2025-05-29 18:46:32 +1000

    update main.py

--- main.py
+++ main.py
@@ -1,91 +1,125 @@
-from pprint import pprint
-
-from src.generators import card_number_generator, filter_by_currency, transaction_descriptions, transactions
-from src.processing import filter_by_state, sort_by_date
-from src.read_transactions import read_transactions_csv, read_transactions_xlsx
-from src.utils import get_transaction
-from src.widget import get_date, mask_account_card
-
-
 def main() -> None:
-    # print(mask_account_card("Maestro 1596837868705199"))
-    #
-    # print(mask_account_card("Счет 64686473678894779589"))
-    #
-    # print(mask_account_card("MasterCard 7158300734726758"))
-    #
-    # print(mask_account_card("Счет 35383033474447895560"))
-    #
-    # print(mask_account_card("Visa Classic 6831982476737658"))
-    #
-    # print(mask_account_card("Visa Platinum 8990922113665229"))
-    #
-    # print(mask_account_card("Visa Gold 5999414228426353"))
-    #
-    # print(mask_account_card("Счет 73654108430135874305"))
-    #
-    # print(get_date("2024-03-11T02:26:18.671407"))
-    #
-    # print(
-    #     filter_by_state(
-    #         [
-    #             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    #             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    #             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    #             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    #         ]
-    #     )
-    # )
-    #
-    # print(
-    #     sort_by_date(
-    #         [
-    #             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    #             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    #             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    #             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    #         ]
-    #     )
-    # )
-    #
-    # print()
-    #
-    # usd_transactions = filter_by_currency(transactions, "USD")
-    # for i in usd_transactions:
-    #     print(i)
-    #
-    # print()
-    #
-    # rub_transactions = filter_by_currency(transactions, "RUB")
-    # for i in rub_transactions:
-    #     print(i)
-    #
-    # print()
-    #
-    # descriptions = transaction_descriptions(transactions)
-    # for i in descriptions:
-    #     print(i)
-    #
-    # print()
-    #
-    # for card_number in card_number_generator(1, 5):
-    #     print(card_number)
-    #
-    # print()
-    #
-    print(get_transaction("data/operations.json"))
-    #
-    # print()
-    l = set()
-    a = read_transactions_csv("data/transactions.csv")
-    descriptions = transaction_descriptions(a)
-    for i in descriptions:
-        l.add(i)
-    print(l)
+    while True:
+        user_answer = input(
+            """
+            Привет! Добро пожаловать в программу работы с банковскими транзакциями.
+        Выберите необходимый пункт меню:\n
+            1. Получить информацию о транзакциях из JSON - файла
+            2. Получить информацию о транзакциях из CSV - файла
+            3. Получить информацию о транзакциях из XLSX - файла\n
+        Выберете пункт: """
+        )
 
-    print(filter_by_state(a))
-    #
-    # pprint(read_transactions_xlsx("data/transactions_excel.xlsx")[:1], indent=4, sort_dicts=False)
+        if user_answer == "1":
+            print(
+                """
+            Для обработки выбран JSON-файл."""
+            )
+            break
+
+        elif user_answer == "2":
+            print(
+                """
+            Для обработки выбран CSV-файл."""
+            )
+            break
+
+        elif user_answer == "3":
+            print(
+                """
+            Для обработки выбран XLSX-файл."""
+            )
+            break
+        else:
+            print(
+                """
+            Некорректный ввод данных"""
+            )
+
+    while True:
+        user_answer_2 = input(
+            """
+        Введите статус, по которому необходимо выполнить фильтрацию.
+    Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING\n
+        Введите статус: """
+        ).upper()
+
+        if user_answer_2 == "EXECUTED":
+            print(
+                f"""
+            Операции отфильтрованы по статусу {user_answer_2}"""
+            )
+            break
+
+        elif user_answer_2 == "CANCELED":
+            print(
+                f"""
+            Операции отфильтрованы по статусу {user_answer_2}"""
+            )
+            break
+
+        elif user_answer_2 == "PENDING":
+            print(
+                f"""
+            Операции отфильтрованы по статусу {user_answer_2}"""
+            )
+            break
+        else:
+            print(
+                f"""
+            Статус операции {user_answer_2} недоступен."""
+            )
+
+    user_answer_3 = (
+        input(
+            """
+        Отсортировать операции по дате? Да/Нет: """
+        )
+        .strip()
+        .lower()
+    )
+    if user_answer_3 == "да":
+        pass
+    else:
+        pass
+
+    user_answer_4 = input(
+        """
+        Отсортировать по возрастанию или по убыванию?: """
+    ).lower()
+    if user_answer_4 == "по возрастанию":
+        pass
+    elif user_answer_4 == "по убыванию":
+        pass
+    else:
+        pass
+
+    user_answer_5 = input(
+        """
+        Выводить только рублевые транзакции? Да/Нет: """
+    ).lower()
+    if user_answer_5 == "да":
+        pass
+    elif user_answer_5 == "нет":
+        pass
+    else:
+        pass
+
+    user_answer_6 = input(
+        """
+        Отфильтровать список транзакций по определенному слову в описании? Да/Нет: """
+    ).lower()
+    if user_answer_6 == "да":
+        user_answer_7 = input(
+            """
+        Введите слово: """
+        )
+    else:
+        pass
+
+    print("Распечатываю итоговый список транзакций...")
+
+    print()
 
 
 if __name__ == "__main__":
